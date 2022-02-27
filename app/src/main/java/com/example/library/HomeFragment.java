4bee859289622c7152d82a279c632039af5a4f96
liaten <vlad171vlad@gmail.com>
2022-02-27 15:27:56 +0300

    дата работает

--- app/src/main/java/com/example/library/HomeFragment.java
+++ app/src/main/java/com/example/library/HomeFragment.java
@@ -12,6 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,24 +24,23 @@ import java.net.URL;
 
 public class HomeFragment extends Fragment {
     DateHelper dateHelper = null;
-    TextView DayOfDate, DayOfWeek, MonthAndYear, Code;
+    TextView DayOfDate, DayOfWeek, MonthAndYear;
     String json_date_result = "";
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         // Находим текстовые поля среди родительского элемента
         DayOfDate = getView().findViewById(R.id.date_day);
         DayOfWeek = getView().findViewById(R.id.day_of_the_week);
         MonthAndYear = getView().findViewById(R.id.month_year);
-        Code = getView().findViewById(R.id.code);
+        //Code = getView().findViewById(R.id.code);
         try {
             String date_url = "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Moscow";
             URL url = new URL(date_url);
             dateHelper = new DateHelper();
             dateHelper.execute(url);
-            Toast.makeText(getActivity(),"date is downloaded",Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getActivity(),"date is downloaded",Toast.LENGTH_SHORT).show();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -50,12 +52,67 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    public String getRussianDaysOfWeek(String dayOfWeek) {
+        switch (dayOfWeek) {
+            case "Sunday":
+                return "Воскресенье";
+            case "Monday":
+                return "Понедельник";
+            case "Tuesday":
+                return "Вторник";
+            case "Wednesday":
+                return "Среда";
+            case "Thursday":
+                return "Четверг";
+            case "Friday":
+                return "Пятница";
+            case "Saturday":
+                return "Суббота";
+        }
+        return null;
+    }
+
+    public String getRussianMonths(int month) {
+        switch (month) {
+            case 1:
+                return "Январь";
+            case 2:
+                return "Февраль";
+            case 3:
+                return "Март";
+            case 4:
+                return "Апрель";
+            case 5:
+                return "Май";
+            case 6:
+                return "Июнь";
+            case 7:
+                return "Июль";
+            case 8:
+                return "Август";
+            case 9:
+                return "Сентябрь";
+            case 10:
+                return "Октябрь";
+            case 11:
+                return "Ноябрь";
+            case 12:
+                return "Декабрь";
+        }
+        return null;
+    }
+
     class DateHelper extends AsyncTask<URL, Fragment, String> {
 
         protected String result = "";
-        protected String day = "";
-        protected String day_of_week = "";
-        protected String month_and_year = "";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            DayOfWeek.setVisibility(View.INVISIBLE);
+            DayOfDate.setVisibility(View.INVISIBLE);
+            MonthAndYear.setVisibility(View.INVISIBLE);
+        }
 
         @Override
         protected String doInBackground(URL... urls) {
@@ -83,10 +140,27 @@ public class HomeFragment extends Fragment {
         @Override
         protected void onPostExecute(String _result) {
             super.onPostExecute(_result);
-            Code.setText(result);
-            json_date_result = result;
+            //Code.setText(result);
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                int day = jsonObject.getInt("day");
+                int year = jsonObject.getInt("year");
+                int month = jsonObject.getInt("month");
+                String dayOfWeek = jsonObject.getString("dayOfWeek");
+                String russianDayOfWeek = getRussianDaysOfWeek(dayOfWeek);
+                String monthAndYear = getRussianMonths(month) + " " + year;
+                DayOfWeek.setText(russianDayOfWeek);
+                DayOfDate.setText(String.valueOf(day));
+                MonthAndYear.setText(monthAndYear);
+                DayOfWeek.setVisibility(View.VISIBLE);
+                DayOfDate.setVisibility(View.VISIBLE);
+                MonthAndYear.setVisibility(View.VISIBLE);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //Log.d(TAG,"Date: " + json_date_result); // Print result to logcat
             dateHelper.cancel(true);
-
         }
     }
+
 }
