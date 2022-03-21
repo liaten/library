21f6885181a8eb2a3078844dd01f290340b3f76b
liaten <vlad171vlad@gmail.com>
2022-03-22 01:39:41 +0300

    speed up

---
+++ app/src/main/java/com/example/library/helper/BookFragmentHelper.java
@@ -0,0 +1,10 @@
+package com.example.library.helper;
+
+import android.os.AsyncTask;
+
+public class BookFragmentHelper extends AsyncTask<Void,Void,Void> {
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
